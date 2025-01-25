abb85c24c30d84d82efdf4516f6cbcfacdc541f6
Georgios Seimenis <giorgosseimenis@gmail.com>
2025-01-25 12:18:48 +0200

    added variable for the radius

--- Assets/Scripts/ProcGenController.cs
+++ Assets/Scripts/ProcGenController.cs
@@ -12,11 +12,16 @@ public class ProcGenController : MonoBehaviour
     // we want this to spawn the bubbles automatically.
     // each one will be spawned in the scene and will have to be correctly spawned in the lines 
     
+    // game object to copy
     [SerializeField] private GameObject prefab;
+    
+    // how many times to copy
     [SerializeField] private int iterations;
     [SerializeField] private float randomYDistanceOffset;
     [SerializeField] private float minXValue;
     [SerializeField] private float maxXValue;
+    [SerializeField] private float minRadius;
+    [SerializeField] private float maxRadius;
     
     private void Start()
     {
@@ -25,6 +30,7 @@ public class ProcGenController : MonoBehaviour
         for (var i = 0; i < iterations; i++)
         {
             var xValue = Random.Range(minXValue, maxXValue);
+            var radius = Random.Range(minRadius, maxRadius);
 
             while (i != 0 && (Math.Abs(previousXValue - xValue) > 3.5f || Math.Abs(previousXValue - xValue) < 1f))
             {
@@ -35,7 +41,7 @@ public class ProcGenController : MonoBehaviour
             
             var prefab = Instantiate(this.prefab, new Vector3(xValue, 6 * i * randomYDistanceOffset, 0), Quaternion.identity);
             prefab.name = $"Bubble {i}";
-            prefab.transform.localScale = new Vector3(0.8f, 0.6f, 0);
+            prefab.transform.localScale = new Vector3(radius, radius, 0);
             
             previousXValue = xValue;
         }
